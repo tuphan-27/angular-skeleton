8527e3fdaff984fb8754815d5159295f98311897
Tu Phan <pt27122000@gmail.com>
2024-07-12 00:42:19 +0700

    chore: config project theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{html,js}'],
-  theme: {
-    extend: {
-      colors: {},
-    },
-  },
-}
+    content: ['./src/**/*.{html,js,ts}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#3B71CA',
+                secondary: '#9FA6B2',
+                sucess: '#14A44D',
+                danger: '#DC4C64',
+                warning: '#E4A11B',
+                info: '#54B4D3',
+                dark: '#332D2D',
+                light: '#FBFBFB'
+            },
+            fontFamily: {
+                primary: ['"Inter Regular"', '"sans-serif"'],
+                'primary-medium': ['"Inter Medium"', '"sans-serif"'],
+                'primary-semibold': ['"Inter Semibold"', '"sans-serif"'],
+
+                secondary: [],
+                'secondary-medium': [],
+                'secondary-semibold': []
+            },
+            screens: {}
+        }
+    }
+};
