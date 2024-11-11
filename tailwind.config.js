ccd38bd1517d519a0990a750edffe5ee363db76e
Tu Phan <pt27122000@gmail.com>
2024-11-11 23:33:06 +0700

    config: migrate to nx

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{html,js,ts}'],
+    content: ['{apps,libs}/*/src/**/*.{html,js,ts}'],
     theme: {
         extend: {
             colors: {
